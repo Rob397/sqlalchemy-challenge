2d510e424b5fbfb1f31ff71f5a2ab179cd576583
Rob397 <105466612+Rob397@users.noreply.github.com>
2022-07-13 12:28:15 +1000

    Adding files
    files and readme

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+
+justice_league_dict = [
+    {"superhero": "Aquaman", "real_name": "Arthur Curry"},
+    {"superhero": "Batman", "real_name": "Bruce Wayne"},
+    {"superhero": "Cyborg", "real_name": "Victor Stone"},
+    {"superhero": "Flash", "real_name": "Barry Allen"},
+    {"superhero": "Green Lantern", "real_name": "Hal Jordan"},
+    {"superhero": "Superman", "real_name": "Clark Kent/Kal-El"},
+    {"superhero": "Wonder Woman", "real_name": "Princess Diana"}
+]
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("")
+def example_():
+    """Return the justice league data as json"""
+
+    return jsonify(justice_league_dict)
